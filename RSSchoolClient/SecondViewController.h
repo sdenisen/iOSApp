84501de5358b8f242e18b461e430d0a98a06cee9
sdenisenko <sdenisen@ymail.com>
2021-05-05 13:39:45 +0300

    Add SecondViewController

---
+++ RSSchoolClient/SecondViewController.h
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.h
+//  RSSchoolClient
+//
+//  Created by Sergey Denisenko on 05.05.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
