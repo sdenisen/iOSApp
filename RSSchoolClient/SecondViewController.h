3308af1eefdb1f517fbdb47ad701a65150f35894
sdenisenko <sdenisen@ymail.com>
2021-05-06 01:13:51 +0300

    init: new UI implemented;

--- RSSchoolClient/SecondViewController.h
+++ RSSchoolClient/SecondViewController.h
@@ -8,7 +8,9 @@
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
-
+//FIXME: test ne
+//TODO: create me
+//MARK: mark me
 @interface SecondViewController : UIViewController
 
 @end
--- RSSchoolClient/ViewController.m
+++ RSSchoolClient/ViewController.m
@@ -8,6 +8,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
@@ -17,6 +20,16 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+- (IBAction)buttonTapped:(id)sender {
+    self.label.text = self.textField.text;
+    [self printLog];
+    self.label.textColor = UIColor.purpleColor;
+}
 
+- (void)printLog{
+    NSLog(@"test1");
+    NSLog(@"test2");
+    NSLog(@"test3");
+}
 
 @end
